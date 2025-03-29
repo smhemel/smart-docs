9fcad23088036f87be9db9fb43d8b49fe3faf7a7
hemel <smhemel.eu@gmail.com>
2025-03-30 03:48:07 +0600

    Create basic editor page with a little bit editor functionality by the help of tiptap package.

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from '@tiptap/starter-kit';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table';
+import Image from '@tiptap/extension-image';
+import ImageResize from "tiptap-extension-resize-image";
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+
+import { useEditorStore } from "@/store/use-editor-store";
+
+interface EditorProps {
+  initialContent?: string | undefined;
+}
+
+const Editor = ({ initialContent }: EditorProps) => {
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onTransaction({ editor }) {
+      setEditor(editor)
+    },
+    onFocus({ editor }) {
+      setEditor(editor)
+    },
+    onBlur({ editor }) {
+      setEditor(editor)
+    },
+    onContentError({ editor }) {
+      setEditor(editor)
+    },
+    editorProps: {
+      attributes: {
+        style: `padding-left: 56px; padding-right: 56px;`,
+        class:
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+    ],
+  });
+
+  return (
+    <div>
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
+
+export default Editor;
